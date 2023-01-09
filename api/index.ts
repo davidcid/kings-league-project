209dae3db9e24c194cef4686589def5053efa321
David Cid <david.cid@gmail.com>
2023-01-09 23:14:48 +0100

    Add scripts and new dependencies

---
+++ api/index.ts
@@ -0,0 +1,17 @@
+import { Hono } from 'hono'
+import leaderboard from '../db/leaderboard.json'
+
+const app = new Hono()
+
+app.get('/', (ctx) => {
+	return ctx.json([{
+		endopoint: '/leaderboard',
+		description: 'Returns the leaderboard'
+	}])
+})
+
+app.get('/leaderboard', (ctx) => {
+	return ctx.json(leaderboard)
+})
+
+export default app
